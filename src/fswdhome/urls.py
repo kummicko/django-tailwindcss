8183c903bbade964f2848844006a4c8a6021a854
Milenko <kummicko@gmail.com>
2023-05-12 20:13:46 +0200

    favicon added

--- src/fswdhome/urls.py
+++ src/fswdhome/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
+# from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     from django.conf.urls.static import static
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
